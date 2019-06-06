16a0ab5b340cf9a61644102c192d3b075f518258
Srinath Kailasa <srinathkailasa@gmail.com>
2019-06-06 17:41:22 +0100

    Fix index coords

--- examples/image_plot.py
+++ examples/image_plot.py
@@ -1,146 +1,139 @@
-import io
 import numpy as np
-import PIL.Image
+import scipy.misc
 
 from chaco.api import (
     ArrayPlotData, CMapImagePlot, DataRange1D, DataRange2D, Greys,
-    GridDataSource, GridMapper, ImageData, Plot, reverse, GridContainer,
-    LinearMapper
+    GridDataSource, GridMapper, ImageData, Plot, reverse, GridPlotContainer,
 )
-from chaco.tools.api import PanTool, ZoomTool, RangeSelection2D, RangeSelectionOverlay
 from chaco.overlays.databox import DataBox
 from enable.component_editor import ComponentEditor
-from enable.tools.resize_tool import ResizeTool
-#from enable.tools.move_tool import MoveTool
 from chaco.tools.api import MoveTool
-from traits.api import Instance, HasTraits, Str, Enum
+from traits.api import Instance, HasTraits, Str, Int, Array
 from traitsui.api import Item, ModelView, View
 
+from chaco.api import ImagePlot
 
-class CustomMoveTool(MoveTool):
-
-    def drag_end(self, event):
-        print("now at position: ", event.x, event.y)
+TEST = scipy.misc.ascent()  # test image
 
 
 def calculate_intensity_histogram(pixel_data):
-    hist, bin_edges = np.histogram(pixel_data.flatten(), bins=1000)
-    bin_width = np.subtract(bin_edges[1], bin_edges[0])
-    return hist, bin_edges[:-1], bin_width
+    #: Number of bins reflect 8-bit greyscale values
+    hist, bin_edges = np.histogram(
+        pixel_data.flatten(), bins=256, range=(0, 256)
+    )
+
+    return hist, bin_edges[:-1]
 
 
-def plot_component(filepath):
+def intensity_histogram_plot_component(im):
+    hist, bin_edges = calculate_intensity_histogram(
+        im.flatten()
+    )
 
-    # load image into array
-    with open(filepath, 'rb') as f:
-        im = PIL.Image.open(io.BytesIO(f.read()))
+    plotdata = ArrayPlotData(x=bin_edges, y=hist)
+    plot = Plot(plotdata)
+    plot.plot(
+        ('x', "y"),
+        type='bar',
+        color='auto',
+        bar_width=1,
+    )
+    # without padding plot just doesn't seem to show up?
+    plot.border_width = 0
+    plot.padding = 0
+    plot.padding_top = 0
+    return plot
 
-    im_arr = np.array(im)
-    height = len(im_arr[0])
-    width = len(im_arr)
 
-    xs = np.arange(0, width)
-    ys = np.arange(0, height)
+def image_histogram_plot_component(im):
+    xs = np.arange(0, len(im))
+    ys = np.arange(0, len(im[0]))
 
-    # define mappers from data to view for chaco
-    index = GridDataSource(xdata=xs, ydata=ys)
+    index = GridDataSource(
+        xdata=xs, ydata=ys, sort_order=('ascending', 'ascending'))
+
     index_mapper = GridMapper(range=DataRange2D(index))
-    
+
     color_source = ImageData(
-            data=np.flipud(im_arr),
-             value_depth=1
+        data=np.flipud(im), value_depth=1
     )
     reversed_grays = reverse(Greys)
     color_mapper = reversed_grays(DataRange1D(color_source))
 
-    # define plot object
-    image_plot = CMapImagePlot(
+    plot = CMapImagePlot(
         index=index,
         index_mapper=index_mapper,
         value=color_source,
-        value_mapper=color_mapper
+        value_mapper=color_mapper,
     )
 
-    # add some tools to plot
+    #: Add overlay for zoom
     data_box_overlay = DataBox(
-        component=image_plot,
+        component=plot,
         data_position=[0, 0],
-        data_bounds=[250,250]
-        )
-    move_tool = CustomMoveTool(
-        component=data_box_overlay
-        )
-    resize_tool = ResizeTool(
-        component=data_box_overlay
-        )
-    data_box_overlay.tools.append(move_tool)
-    #data_box_overlay.tools.append(resize_tool)
-
-    image_plot.overlays.append(data_box_overlay)
-
-    # Define intensity histogram plot
-    (hist,
-    bin_edges,
-    bin_width) = calculate_intensity_histogram(
-        im_arr.flatten()
+        data_bounds=[50, 50],
     )
-    hist_plot_data = ArrayPlotData(x=bin_edges, y=hist)
 
-    hist_plot = Plot(hist_plot_data)
-    hist_plot.plot(("x", "y"), type='bar', color='auto', bar_width=bin_width)
+    move_tool = MoveTool(component=data_box_overlay)
+    data_box_overlay.tools.append(move_tool)
 
-    hist_plot.tools.append(
-        PanTool(
-            hist_plot
-            )
-        )
+    data_box_overlay.on_trait_change(MyImagePlot.printer, 'data_position')
 
-    # without padding plot just doesn't seem to show up?
-    hist_plot.border_width = 1
-    hist_plot.padding = 0
-    hist_plot.padding_top = 1
+    #: Add to plot
+    plot.overlays.append(data_box_overlay)
 
-    # define container
-    # shape must match number of plots
-    container = GridContainer(padding=40, fill_padding=True,
-                              use_backbuffer=True, shape=(1,2),
-                              spacing=(20,20))
+    return plot
 
-    container.add(hist_plot)
-    container.add(image_plot)
+
+def grid_plot_component(im, *plot_constructors):
+
+    container = GridPlotContainer(
+        padding=50,
+        fill_padding=True,
+        shape=(1, len(plot_constructors)),
+        spacing=(20, 20)
+    )
+
+    for plot_constructor in plot_constructors:
+        plot = plot_constructor(im)
+        container.add(plot)
 
     return container
 
 
-class ImagePlot(HasTraits):
-
-    # file to load
-    filepath = Str()
+class MyImagePlot(HasTraits):
 
     # define plot as a trait
-    plot = Instance(GridContainer)
+    plot = Instance(GridPlotContainer)
+
+    position = Array(shape=(2, 1))
 
     traits_view = View(
         Item(
             'plot',
             editor=ComponentEditor(),
             ),
-            resizable=True, title='Image:',
-            width=800, height=600
+        title='Image:',
+        resizable=True,
         )
 
+    plot_constructors = [
+        image_histogram_plot_component,
+        intensity_histogram_plot_component,
+    ]
+
     def _plot_default(self):
-        return plot_component(self.filepath)
+        return grid_plot_component(TEST, *self.plot_constructors)
+
+    def printer(self, name, new):
+
+        self.position = np.array([self.x, self.y])
+
+        print("from the plot!: ", self.position)
+        print("in index coords", self.component.map_index(self.position))
 
 
 if __name__ == "__main__":
-    import sys
-
-    filepath = 'lena512.bmp'
-    if len(sys.argv) > 2:
-        filepath = sys.argv[2]
-    image = ImagePlot()
-
-    image.filepath = filepath
-
+    image = MyImagePlot()
     image.configure_traits()
+
