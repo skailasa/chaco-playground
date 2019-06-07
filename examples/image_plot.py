c98e3b78ff9b5ad4405c0438166b23addd55c05f
Srinath Kailasa <srinathkailasa@gmail.com>
2019-06-07 11:48:08 +0100

    Fix position listener

--- examples/image_plot.py
+++ examples/image_plot.py
@@ -8,12 +8,19 @@ from chaco.api import (
 from chaco.overlays.databox import DataBox
 from enable.component_editor import ComponentEditor
 from chaco.tools.api import MoveTool
-from traits.api import Instance, HasTraits, Str, Int, Array
+from traits.api import Instance, HasTraits, Str, Int, Array, on_trait_change, Event, Tuple
 from traitsui.api import Item, ModelView, View
 
-from chaco.api import ImagePlot
 
 TEST = scipy.misc.ascent()  # test image
+
+
+class MyDataBox(DataBox):
+
+    updated = Event()
+
+    def _data_position_changed(self):
+        self.updated = True
 
 
 def calculate_intensity_histogram(pixel_data):
@@ -25,88 +32,12 @@ def calculate_intensity_histogram(pixel_data):
     return hist, bin_edges[:-1]
 
 
-def intensity_histogram_plot_component(im):
-    hist, bin_edges = calculate_intensity_histogram(
-        im.flatten()
-    )
-
-    plotdata = ArrayPlotData(x=bin_edges, y=hist)
-    plot = Plot(plotdata)
-    plot.plot(
-        ('x', "y"),
-        type='bar',
-        color='auto',
-        bar_width=1,
-    )
-    # without padding plot just doesn't seem to show up?
-    plot.border_width = 0
-    plot.padding = 0
-    plot.padding_top = 0
-    return plot
-
-
-def image_histogram_plot_component(im):
-    xs = np.arange(0, len(im))
-    ys = np.arange(0, len(im[0]))
-
-    index = GridDataSource(
-        xdata=xs, ydata=ys, sort_order=('ascending', 'ascending'))
-
-    index_mapper = GridMapper(range=DataRange2D(index))
-
-    color_source = ImageData(
-        data=np.flipud(im), value_depth=1
-    )
-    reversed_grays = reverse(Greys)
-    color_mapper = reversed_grays(DataRange1D(color_source))
-
-    plot = CMapImagePlot(
-        index=index,
-        index_mapper=index_mapper,
-        value=color_source,
-        value_mapper=color_mapper,
-    )
-
-    #: Add overlay for zoom
-    data_box_overlay = DataBox(
-        component=plot,
-        data_position=[0, 0],
-        data_bounds=[50, 50],
-    )
-
-    move_tool = MoveTool(component=data_box_overlay)
-    data_box_overlay.tools.append(move_tool)
-
-    data_box_overlay.on_trait_change(MyImagePlot.printer, 'data_position')
-
-    #: Add to plot
-    plot.overlays.append(data_box_overlay)
-
-    return plot
-
-
-def grid_plot_component(im, *plot_constructors):
-
-    container = GridPlotContainer(
-        padding=50,
-        fill_padding=True,
-        shape=(1, len(plot_constructors)),
-        spacing=(20, 20)
-    )
-
-    for plot_constructor in plot_constructors:
-        plot = plot_constructor(im)
-        container.add(plot)
-
-    return container
-
-
 class MyImagePlot(HasTraits):
 
     # define plot as a trait
     plot = Instance(GridPlotContainer)
 
-    position = Array(shape=(2, 1))
+    position = Tuple(0, 0)
 
     traits_view = View(
         Item(
@@ -117,23 +48,94 @@ class MyImagePlot(HasTraits):
         resizable=True,
         )
 
-    plot_constructors = [
-        image_histogram_plot_component,
-        intensity_histogram_plot_component,
-    ]
+    def __init__(self):
+        self.plot_constructors = [
+            self.image_histogram_plot_component,
+            self.intensity_histogram_plot_component,
+        ]
 
     def _plot_default(self):
-        return grid_plot_component(TEST, *self.plot_constructors)
+        return self.grid_plot_component()
 
-    def printer(self, name, new):
+    def grid_plot_component(self):
+        container = GridPlotContainer(
+            padding=50,
+            fill_padding=True,
+            shape=(1, len(self.plot_constructors)),
+            spacing=(20, 20)
+        )
 
-        self.position = np.array([self.x, self.y])
+        for plot_constructor in self.plot_constructors:
+            plot = plot_constructor()
+            container.add(plot)
 
-        print("from the plot!: ", self.position)
-        print("in index coords", self.component.map_index(self.position))
+        return container
+
+    def image_histogram_plot_component(self):
+        xs = np.arange(0, len(TEST))
+        ys = np.arange(0, len(TEST))
+
+        index = GridDataSource(
+            xdata=xs, ydata=ys, sort_order=('ascending', 'ascending'))
+
+        index_mapper = GridMapper(range=DataRange2D(index))
+
+        color_source = ImageData(
+            data=np.flipud(TEST), value_depth=1
+        )
+        reversed_grays = reverse(Greys)
+        color_mapper = reversed_grays(DataRange1D(color_source))
+
+        plot = CMapImagePlot(
+            index=index,
+            index_mapper=index_mapper,
+            value=color_source,
+            value_mapper=color_mapper,
+        )
+
+        #: Add overlay for zoom
+        data_box_overlay = MyDataBox(
+            component=plot,
+            data_position=[0, 0],
+            data_bounds=[50, 50],
+        )
+
+        move_tool = MoveTool(component=data_box_overlay)
+        data_box_overlay.tools.append(move_tool)
+
+        data_box_overlay.on_trait_change(self.update_position, 'position')
+
+        #: Add to plot
+        plot.overlays.append(data_box_overlay)
+
+        return plot
+
+    def intensity_histogram_plot_component(self):
+        hist, bin_edges = calculate_intensity_histogram(
+            TEST.flatten()
+        )
+
+        plotdata = ArrayPlotData(x=bin_edges, y=hist)
+        plot = Plot(plotdata)
+        plot.plot(
+            ('x', "y"),
+            type='bar',
+            color='auto',
+            bar_width=1,
+        )
+        # without padding plot just doesn't seem to show up?
+        plot.border_width = 0
+        plot.padding = 0
+        plot.padding_top = 0
+        return plot
+
+    def update_position(self, name, new):
+        self.position = self.plot._components[0].map_index(new)
+
+    def _position_changed(self):
+        print(self.position)
 
 
 if __name__ == "__main__":
     image = MyImagePlot()
     image.configure_traits()
-
